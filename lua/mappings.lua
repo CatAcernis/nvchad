5e3cbcc93fb5d4786665cc668b37fb01989e2cde
CatAcernis <real.lxm@gmail.com>
2024-04-16 21:22:08 +1000

    added mappings for buffer navigations

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- buffer move
+map("n", "<S-h>", ":bprevious <CR>")
+map("n", "<S-l>", ":bnext <CR>")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
