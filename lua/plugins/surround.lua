93d3148e61c91ab2b290615838f66779e3b26b50
CatAcernis <real.lxm@gmail.com>
2024-04-17 02:18:30 +1000

    updated surround config

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,6 +1,7 @@
 return {
   {
     "kylechui/nvim-surround",
+    lazy = false,
     config = function()
       require("nvim-surround").setup({
         -- Configuration here, or leave empty to use defaults
