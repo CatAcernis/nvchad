3f7e4069a93c9715d4dbef41e8e6887edb32b122
simon liu <real.lxm@gmail.com>
2024-04-20 16:26:44 +1000

    updated neovide config

--- lua/options.lua
+++ lua/options.lua
@@ -8,3 +8,15 @@ o.cursorlineopt ='both' -- to enable cursorline!
 local opt = vim.opt
 opt.number = true
 opt.relativenumber = true
+
+local g = vim.g
+
+-- neovide configs
+if g.neovide then
+  g.neovide_transparency = 0.85
+  g.neovide_refresh_rate = 144
+  g.neovide_cursor_vfx_mode = 'ripple'
+  g.neovide_cursor_animation_length = 0.03
+  g.neovide_cursor_trail_size = 0.9
+  g.neovide_remember_window_size = true
+end
