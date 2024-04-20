771d21fe383ed1465a31683e41dfae230627846d
simon liu <real.lxm@gmail.com>
2024-04-20 16:51:41 +1000

    added config to remap alt to meta on macos in neovide

--- lua/options.lua
+++ lua/options.lua
@@ -19,4 +19,5 @@ if g.neovide then
   g.neovide_cursor_animation_length = 0.03
   g.neovide_cursor_trail_size = 0.9
   g.neovide_remember_window_size = true
+  g.neovide_input_macos_alt_is_meta = true
 end
